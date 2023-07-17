e6fc69b50766c556d2f8d06a3bd1229e14cae573
MadeleineCydeo <consciousbubbling@gmail.com>
2023-07-17 00:34:42 -0700

    Initial commit

---
+++ src/test/java/com/blue_CRM/runners/CukesRunner.java
@@ -0,0 +1,27 @@
+package com.blue_CRM.runners;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+
+        plugin = {
+                "html:target/cucumber-reports.html",
+                "rerun:target/rerun.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+        },
+        features = "src/test/resources/features",
+        glue = "com/blue_CRM/step_definitions",
+        dryRun = false,
+        tags = " @wip",
+        publish = true //generating a report with public link
+)
+
+
+
+
+
+public class CukesRunner {}
---
+++ src/test/java/com/blue_CRM/runners/FailedTestRunner.java
@@ -0,0 +1,21 @@
+package com.blue_CRM.runners;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+
+        features = "@target/rerun.txt",
+        glue = "com/blue_CRM/step_definitions"
+)
+
+
+
+
+
+public class FailedTestRunner {}
